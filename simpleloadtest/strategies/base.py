7084ef1e262e609a06ab883ecc6d97e7704db56d
Or Levi <orlevi128@gmail.com>
2020-08-29 18:45:05 +0300

    tests

--- simpleloadtest/strategies/base.py
+++ simpleloadtest/strategies/base.py
@@ -11,42 +11,46 @@ class StrategyBase:
         self.ps_workers = []
 
     def start(self, raw_payloads):
-        print('Initializing inputs...')
-        inputs = self.init_inputs(raw_payloads)
+        try:
+            print('Initializing inputs...')
+            inputs = self.init_inputs(raw_payloads)
 
-        print('Starting up workers...')
-        before = datetime.utcnow()
-        result_queue = mp.Queue()
-        for i in range(self.workers):
-            p = self.spawn_worker(worker_id=i + 1,
-                                  inputs=inputs,
-                                  result_queue=result_queue)
-            self.ps_workers.append(p)
+            print('Starting up workers...')
+            before = datetime.utcnow()
+            result_queue = mp.Queue()
+            for i in range(self.workers):
+                p = self.spawn_worker(worker_id=i + 1,
+                                      inputs=inputs,
+                                      result_queue=result_queue)
+                self.ps_workers.append(p)
 
-        self.join_workers()
-        for p in self.ps_workers:
-            p.terminate()
+            self.join_workers()
+            for p in self.ps_workers:
+                p.terminate()
 
-        total_time_delta = datetime.utcnow() - before
-        total_time_seconds = total_time_delta.total_seconds()
+            total_time_delta = datetime.utcnow() - before
+            total_time_seconds = total_time_delta.total_seconds()
 
-        times = []
+            times = []
 
-        error_count = 0
-        total_requests = 0
-        while not result_queue.empty():
-            total_requests += 1
-            result, pid = result_queue.get()
-            if result.get('error'):
-                error_count += 1
-            else:
-                times.append(result['request_time_ms'])
+            error_count = 0
+            total_requests = 0
+            while not result_queue.empty():
+                total_requests += 1
+                result, pid = result_queue.get()
+                if result.get('error'):
+                    error_count += 1
+                else:
+                    times.append(result['request_time_ms'])
 
-        return Stats(workers=self.workers,
-                     total_requests=total_requests,
-                     total_time_sec=total_time_seconds,
-                     times=times,
-                     error_count=error_count)
+            return Stats(workers=self.workers,
+                         total_requests=total_requests,
+                         total_time_sec=total_time_seconds,
+                         times=times,
+                         error_count=error_count)
+        except Exception as e:
+            for p in self.ps_workers:
+                p.terminate()
 
     @abstractmethod
     def init_inputs(self, raw_payloads):
